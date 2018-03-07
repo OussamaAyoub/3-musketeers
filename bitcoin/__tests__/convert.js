eebdc9189353e92ad5493148b137a2ad5c2a3585
OussamaAyoub <32196617+OussamaAyoub@users.noreply.github.com>
2018-03-07 15:57:27 +0100

    Modified fex test

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -4,19 +4,19 @@ const convert = require('..');
 const Big = require('big.js');
 
 test('should default to returning a Number', () => {
-  //convert(2, 'BTC', 'BTC');
+  expect(convert(2, 'BTC', 'BTC')).toBe(2);
 });
 
 test('should return a Number', () => {
-  //convert(2, 'BTC', 'BTC', 'Number');
+  expect(typeof(convert(2, 'BTC', 'BTC', 'Number'))).toBe("number");
 });
 
 test('should return a Big number', () => {
-  //convert(2, 'BTC', 'BTC', 'Big');
+  //expect(typeof(convert(2, 'BTC', 'BTC', 'Big'))).toBe("big");
 });
 
 test('should return a String', () => {
-  //convert(2100, 'mBTC', 'BTC', 'String');
+  expect(typeof(convert(2100, 'mBTC', 'BTC', 'String'))).toBe("string");
 });
 
 test('should convert an integer', () => {
@@ -36,17 +36,17 @@ test('should convert a Big number', () => {
 });
 
 test('should convert a NaN to a Number', () => {
-  //convert(NaN, 'BTC', 'BTC', 'Number');
-  //convert(NaN, 'BTC', 'mBTC', 'Number');
+ 	//convert(NaN, 'BTC', 'BTC', 'Number');
+  	//convert(NaN, 'BTC', 'mBTC', 'Number');
 });
 
 test('should convert a NaN to a String', () => {
-  //convert(NaN, 'BTC', 'BTC', 'String');
-  //convert(NaN, 'BTC', 'mBTC', 'String');
+  	expect(typeof(convert(NaN, 'BTC', 'BTC', 'String'))).toBe("string");
+  	expect(typeof(convert(NaN, 'BTC', 'mBTC', 'String'))).toBe("string");
 });
 
 test('should not convert a NaN to a Big', () => {
-  //convert(NaN, 'BTC', 'BTC', 'Big');
+  expect(convert(NaN, 'BTC', 'BTC', 'Big')).toThrow();
 });
 
 test('should handle rounding errors', () => {
@@ -62,8 +62,8 @@ test('should throw when untest is undefined', () => {
 });
 
 test('should throw when representaion is undefined', () => {
-  //convert(2, 'BTC', 'mBTC', 'x');
-  //convert(NaN, 'BTC', 'mBTC', 'x');
+  expect(convert(2, 'BTC', 'mBTC', 'x')).toThrow();
+  expect(convert(NaN, 'BTC', 'mBTC', 'x')).toThrow();
 });
 
 test('should allow untest aliases', () => {
